72151cdae8b0beb21ac0194032dd9ff2c1ce4d93
Matt Karmazyn <mkarmazyn@leadid.com>
2015-03-02 10:35:28 -0500

    update

--- crib_ricker/Assets/Scripts/master_script.cs
+++ crib_ricker/Assets/Scripts/master_script.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class master_script : MonoBehaviour {
 
+	public Text score_text;
+	public Text paddles_text;
 	public int score;
 	public int paddles;
 	private int status;
@@ -11,26 +14,42 @@ public class master_script : MonoBehaviour {
 	void Start () {
 		score = 0;
 		paddles = 5;
+		update_paddle_text ();
+		update_score_text ();
 	}
-
+	
 	// Add number of paddles
 	void add_paddle (int addition) {
 		paddles += addition;
+		update_paddle_text ();
 	}
 
-	// subtract number of paddles
+	// Subtract number of paddles
 	void subtract_paddle (int subtraction) {
 		paddles -= subtraction;
+		update_paddle_text ();
 	}
 
-	// add score
+	// Add score
 	void add_points (int addition) {
 		score += addition;
+		update_score_text ();
 	}
 
-	// subtract number of paddles
+	// Subtract score
 	void subtract_points (int subtraction) {
 		score -= subtraction;
+		update_score_text ();
+	}
+	
+	// Updates the paddles Text UI element
+	void update_paddle_text () {
+		paddles_text.text = "Paddles: " + paddles;
+	}
+
+	// Updates the score Text UI element
+	void update_score_text () {
+		score_text.text = "Score: " + score;
 	}
 
 	// Game Over
