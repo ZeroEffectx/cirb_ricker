ad4360457ab0e34e1a45b38a6ccbd4f62a678d6b
Matt Karmazyn <mkarmazyn@leadid.com>
2015-03-02 00:34:06 -0500

    update

---
+++ crib_ricker/Assets/Scripts/ball_script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball_script : MonoBehaviour {
+
+	private GameObject master;
+
+	// Use this for initialization
+	void Awake () {
+		master = GameObject.FindGameObjectWithTag ("master");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movement logic goes here.
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		// Increment paddle and score, then destroy other
+		if (other.gameObject.tag == "box") {
+			master.gameObject.SendMessage ("add_points", 1);
+			master.gameObject.SendMessage ("add_paddle", 1);
+			Destroy (other.gameObject);
+		}
+	}
+
+}
